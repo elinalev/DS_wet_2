253c7ca606e0d5063fd606d172b1c1fd1a2ca613
Elina Levi <elinalevi2@gmail.com>
2022-01-01 19:44:03 +0200

    add files

---
+++ PlayersManager.cpp
@@ -0,0 +1,10 @@
+#include"library2.h"
+#include"PlayersManager.h"
+
+void* Init() {
+    PlayersManager *DS = new PlayersManager();
+    return (void*)DS;
+}
+StatusType AddGroup(void *DS, int GroupID){
+    return ((PlayersManager*)DS)-> AddGroup (GroupID);
+}
