158148ad401bc9749ce97bacaf6982f3e1597d7d
Elina Levi <elinalevi2@gmail.com>
2022-01-06 22:09:25 +0200

    fixed utils files

--- utils.h
+++ utils.h
@@ -1,7 +1,23 @@
+//
+// Created by lior1 on 1/2/2022.
+//
+
 #ifndef DS_WET_2_UTILS_H
 #define DS_WET_2_UTILS_H
 
-int max(int a, int b);
-int find_pow_of_2(int n);
+int max (int x, int y) {
+    return x>y? x : y;
+}
 
+int find_height(int n){
+    if(n == 1)
+        return 0;
+    return 1 + find_height(n/2);
+}
+
+int find_pow_of_2(int n){
+    int i = 1;
+    for(; i  <=  n; i *=2){}
+    return i;
+}
 #endif //DS_WET_2_UTILS_H
